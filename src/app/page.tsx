feb1e11d85fc1dd128143965a0ea753a99df3c06
zaktech96 <zakariyesahid96@gmail.com>
2024-01-03 14:37:45 +0000

    Add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen p-24">
       <h1 className="text-5xl font-bold">Job Board</h1>
+      <p>
+        Hello
+      </p>
     </main>
   );
 }
