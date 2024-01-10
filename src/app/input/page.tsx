2a97fd5ffac8bb33868e0658bc8c4b9827da5a59
zaktech96 <zakariyesahid96@gmail.com>
2024-01-10 19:57:42 +0000

    routing applied

--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -1,21 +1,13 @@
 
-import Nav from '@/components/nav';
-export default function Home() {
+export default function Input() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
-      <Nav />
+
       <h1 className="text-5xl font-bold mb-4">Job Board</h1>
       <p className="text-lg">
         Welcome to our Job Board! Find exciting opportunities and take the next
         step in your career Journey.
       </p>
-      
     </main>
-    // create button to rooute to input page using event handler
-
-    
-
   );
 }
-
-
