9415ac672a6b360286c04c0c923ec653f57ca1ac
zaktech96 <zakariyesahid96@gmail.com>
2024-01-11 16:41:32 +0000

    Routing Applied with input component

--- src/app/Test/page.tsx
+++ src/app/Test/page.tsx
@@ -1,12 +1,16 @@
-export default function Input() {
+import { Input } from "@/components/ui/input"
+
+export default function Inputs() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
 
-      <h1 className="text-5xl font-bold mb-4">Job Board</h1>
+      {/* <h1 className="text-5xl font-bold mb-4">Job Board</h1>
       <p className="text-lg">
         Welcome to our Job Board! Find exciting opportunities and take the next
         step in your career Journey.
-      </p>
+      </p> */}
+      <Input />
+
     </main>
   );
 }
