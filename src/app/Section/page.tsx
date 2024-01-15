47ffab996c98a323023d28297bf3e62de172bbf9
zaktech96 <zakariyesahid96@gmail.com>
2024-01-15 19:20:21 +0000

    Routing Applied adding input page

--- src/app/Section/page.tsx
+++ src/app/Section/page.tsx
@@ -2,7 +2,8 @@
 export default function Section() {
     return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
-         <h1 className="text-5xl font-bold mb-4">Job Board</h1>
+        <h1 className="text-5xl font-bold mb-4">Job Board</h1>
+    
     </main>
   );
 }
--- src/app/Test/page.tsx
+++ src/app/Test/page.tsx
@@ -14,7 +14,7 @@ export default function Inputs() {
         
          <Link href="/Section"> 
         <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Go to Input Page
+          Go to Input Pages
         </button>
       </Link>
     </main>
